644367a124c75cf1057b74a2970c5e7b570161a6
hanshantong <1356466973@qq.com>
2018-08-10 16:25:31 +0800

    two dimentison and three dimentison array..

--- vectorArray.cpp
+++ vectorArray.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 int main(int argc,char** argv)
 {
+    /*#define two dimension array*/
     vector<vector<int>> arr(3,vector<int>(2,0));
     arr[0][0] = 10;
     arr[0][1] = 20;
@@ -23,6 +24,7 @@ int main(int argc,char** argv)
     arr[2][0] = 100;
     arr[2][1] = 200;
 
+    /*#Loop by iterator*/
     for(vector<vector<int>> ::iterator it = arr.begin(); it != arr.end(); ++it)
     {
         for(vector<int>::iterator iti = (*it).begin(); iti != (*it).end(); ++iti)
@@ -32,6 +34,36 @@ int main(int argc,char** argv)
         cout << endl;
 
     }
+
+    /*#define three dimension array*/
+    vector<vector<vector<float>>> data(5,vector<vector<float>>(4,vector<float>(3,1.0f)));
+    //above definition is equal to:
+
+    vector<float> v1(3,2.0f);
+    vector<vector<float>> v2(4,v1);
+    vector<vector<vector<float>>> v3(5,v2);
+
+    v3[0][0][0] = 5;
+    v3[1][0][0] = 10;
+    v3[2][0][0] = 15;
+    v3[3][0][0] = 20;
+    v3[4][0][0] = 25;
+
+    cout << endl;
+    for(int i = 0; i < v3.size(); ++i)
+    {
+        for(int j = 0; j < v2.size(); ++j)
+        {
+            for(int k = 0; k < v1.size(); ++k)
+            {
+                cout << v3[i][j][k] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    vector<int> vv();
+
 
     return 0;
 }
